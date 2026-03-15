745eef13c3021fb33de947ec1fc912f2b50dc5e4
Lars Wischhof <wischhof@ieee.org>
2026-03-15 22:35:16 +0100

    adaptation to inet 4.6.0

--- subprojects/veins_inet/src/veins_inet/VeinsInetApplicationBase.cc
+++ subprojects/veins_inet/src/veins_inet/VeinsInetApplicationBase.cc
@@ -34,7 +34,7 @@
 #include "inet/common/TimeTag_m.h"
 #include "inet/networklayer/common/L3AddressResolver.h"
 #include "inet/networklayer/common/L3AddressTag_m.h"
-#include "inet/transportlayer/contract/udp/UdpControlInfo_m.h"
+#include "inet/transportlayer/contract/udp/UdpCommand_m.h"
 
 namespace veins {
 
